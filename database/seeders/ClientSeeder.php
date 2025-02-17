d5d474038f6e216954a61f4e2e98c1414074203d
João Marcos Neves da Silva <58483603+joaomarcosns@users.noreply.github.com>
2025-02-17 08:10:27 -0300

    refactor: Client Seeder refactor

--- database/seeders/ClientSeeder.php
+++ database/seeders/ClientSeeder.php
@@ -6,39 +6,50 @@ namespace Database\Seeders;
 
 use App\Enums\UserRoleEnum;
 use App\Models\Client;
+use App\Models\Team;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class ClientSeeder extends Seeder
 {
     /** Run the database seeds. */
     public function run(): void
     {
-        Client::factory()
-            ->hasTeams(3)
-            ->hasBuildings(5)
-            ->create();
+        // Create two clients
+        foreach (range(1, 2) as $index) {
+            $this->createClientWithTeamsAndUsers();
+        }
+    }
 
-        $client = Client::first();
+    private function createClientWithTeamsAndUsers(): void
+    {
+        // Create a client
+        $client = Client::factory()->hasBuildings(5)->create();
 
-        $teams = $client->teams;
-
-        foreach ($teams as $team) {
-            User::factory()
-                ->count(3)
-                ->create([
-                    'team_id' => $team->id,
-                    'client_id' => $client->id,
-                ]);
+        if (!$client) {
+            $this->command->error('No client was created.');
+            return;
         }
 
-        $user = User::first();
+        // Create predefined teams
+        $teams = collect(['Electrician', 'Plumber', 'Doorman'])
+            ->map(fn ($name) => Team::create(['name' => $name, 'client_id' => $client->id]));
 
-        $user->update([
-            'email' => 'teste@email.com',
-            'password' => bcrypt('123456'),
-            'role' => UserRoleEnum::OWNER,
-            'team_id' => null,
-        ]);
+        // Create users for each team
+        $teams->each(fn ($team) => User::factory()->count(3)->create([
+            'team_id' => $team->id,
+            'client_id' => $client->id,
+        ]));
+
+        // Set a user as OWNER (without a team)
+        if ($firstUser = User::where('client_id', $client->id)->first()) {
+            $firstUser->update([
+                'email' => "teste@email{$client->id}.com",
+                'password' => Hash::make('123456'),
+                'role' => UserRoleEnum::OWNER,
+                'team_id' => null,
+            ]);
+        }
     }
 }
